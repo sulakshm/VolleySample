20e2fab68fa6c2f8b77f9c0948be821497da9d5c
sulakshm <lns.myspace@gmail.com>
2015-04-10 18:34:46 +0530

    remove old sources

--- src/main/java/com/gandiva/sulakshm/volleysample/FilePickerResponse.java
+++
@@ -1,8 +0,0 @@
-package com.gandiva.sulakshm.volleysample;
-
-/**
- * Created by LakshmiNarasimhan on 4/10/2015.
- */
-public interface FilePickerResponse {
-    void onSuccess(String path, String file);
-}
